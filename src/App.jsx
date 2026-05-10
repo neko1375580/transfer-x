37ba2d4f948e50a20fa0d8899edb13d67ee0776d
Neko <legendata27301@gmail.com>
2026-05-10 17:42:04 +0300

    FIX QR

--- src/App.jsx
+++ src/App.jsx
@@ -482,7 +482,7 @@ function App(){
                 <div className="qrBox">
 
                   <QRCodeCanvas
-                    value="https://transfer-x.onrender.com"
+                    value="value="https://transfer-x-lyart.vercel.app""
                     size={230}
                     bgColor="#0f172a"
                     fgColor="#ffffff"
